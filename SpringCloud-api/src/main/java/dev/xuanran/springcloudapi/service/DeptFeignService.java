16e03603077c7495dc9c3d0958930b30266d605f
XuanRanDev <3135419729@qq.com>
2022-08-21 00:26:46 +0800

    feign接口调用支持统一前缀

--- SpringCloud-api/src/main/java/dev/xuanran/springcloudapi/service/DeptFeignService.java
+++ SpringCloud-api/src/main/java/dev/xuanran/springcloudapi/service/DeptFeignService.java
@@ -2,19 +2,17 @@ package dev.xuanran.springcloudapi.service;
 
 import dev.xuanran.springcloudapi.pojo.Dept;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@FeignClient(value = "SPRINGCLOUD-PROVIDER-DEPT")
+@FeignClient(value = "SPRINGCLOUD-PROVIDER-DEPT",path = "/provider")
 public interface DeptFeignService {
 
-    @GetMapping("/provider/getDeptById/{id}")
+    @GetMapping("/getDeptById/{id}")
     Dept getDeptById(@PathVariable("id") int id);
 
-    @GetMapping("/provider/getDeptList")
+    @GetMapping("/getDeptList")
     List<Dept> getDeptList();
 }
